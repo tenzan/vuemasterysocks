a84c807ce156364f512e05c2aa436ad440b5b843
Askarbek Karasasev <askar.karasaev@gmail.com>
2018-04-30 23:19:19 +0900

    images replaced

--- main.js
+++ main.js
@@ -2,19 +2,19 @@ var app = new Vue({
     el: "#app",
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green.png",
+        image: "./assets/vmSocks-green.jpg",
         inStock: false,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: "./assets/vmSocks-green.png"
+                variantImage: "./assets/vmSocks-green.jpg"
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: "./assets/vmSocks-blue.png"
+                variantImage: "./assets/vmSocks-blue.jpg"
             }
         ],
         cart: 0
